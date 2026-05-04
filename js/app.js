ec999f667c3964c31045c4edce23216e193ca6f3
Ayelen Borges <148565107+Luminae-Studio@users.noreply.github.com>
2026-05-04 19:16:41 -0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,10 +1,27 @@
 // FISCHER METHOD -- app.js
+// Versao: 2026-05-04
+
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Remove service workers antigos que estao cacheando versoes velhas
+  // Remove TODOS os service workers e caches antigos
   if ('serviceWorker' in navigator) {
+    // Desregistra todos os SWs
     navigator.serviceWorker.getRegistrations().then(function(regs) {
-      regs.forEach(function(reg) { reg.unregister(); });
+      var promises = regs.map(function(reg) {
+        console.log('Removendo SW:', reg.scope);
+        return reg.unregister();
+      });
+      return Promise.all(promises);
+    }).then(function() {
+      // Limpa todos os caches
+      return caches.keys().then(function(keys) {
+        return Promise.all(keys.map(function(k) {
+          console.log('Limpando cache:', k);
+          return caches.delete(k);
+        }));
+      });
+    }).catch(function(err) {
+      console.log('SW cleanup error:', err);
     });
   }
 
