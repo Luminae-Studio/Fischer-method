ecd3c9f395cf90e975daca208cacd94246b8a761
Ayelen Borges <148565107+Luminae-Studio@users.noreply.github.com>
2026-05-07 12:05:00 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,9 @@
-// FISCHER METHOD -- sw.js v4
-// Service worker minimo - sem cache de JS/CSS
-// Apenas remove caches antigos
-
-self.addEventListener('install', function(e) {
-  self.skipWaiting();
-});
-
+// FISCHER METHOD -- sw.js
+self.addEventListener('install', function() { self.skipWaiting(); });
 self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keys) {
-      return Promise.all(keys.map(function(k) {
-        console.log('Deletando cache:', k);
-        return caches.delete(k);
-      }));
-    }).then(function() {
-      return self.clients.claim();
-    })
+      return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+    }).then(function() { return self.clients.claim(); })
   );
 });
-
-// SEM interceptacao de fetch
-// Todos os arquivos vem sempre da rede
